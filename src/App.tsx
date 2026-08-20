ed5b37dc61b0b3f070710af66785dacca1e7c7e9
Jean Paul TUYISENGE <JeanPaul.Tuyisenge@rra.gov.rw>
2026-08-20 11:43:40 +0200

    ft-added header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Header } from "./components/Header";
+
 export default function App() {
-  return <h1 className="text-2xl font-bold">Habit Tracker</h1>;
+  return (
+    <div className="max-w-2xl mx-auto flex flex-col gap-4 p-4">
+      <Header />
+    </div>
+  );
 }
