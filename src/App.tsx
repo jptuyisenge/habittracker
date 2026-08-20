9acdd6355bc0494eeb3bfd482cd3053b997f4186
Jean Paul TUYISENGE <JeanPaul.Tuyisenge@rra.gov.rw>
2026-08-20 11:56:51 +0200

    ft-added a habit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { HabitForm } from "./components/HabitForm";
 
 export default function App() {
   return (
     <div className="max-w-2xl mx-auto flex flex-col gap-4 p-4">
       <Header />
+      <HabitForm />
     </div>
   );
 }
